43e56a96d034331242a6a77649401513b3a0a991
Sophie Dubois <sophiemargueritedubois@gmail.com>
2019-10-08 15:11:02 -0400

    Added the functionality for volume and videoplayback speed ranges as well as the skip forward/backward buttons

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -4,7 +4,7 @@ const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelectorAll(['data-skip']);
+const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider')
 
 // Step 2: Build our functions
@@ -18,10 +18,29 @@ function updateButton(e) {
     toggle.textContent = icon;
 }
 
+function skip() {
+    skipSecs = parseFloat(this.dataset.skip);
+    video.currentTime += skipSecs;
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
 // Step 3: Hook up event listeners
 toggle.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
+
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+
+skipButtons.forEach(function(button){
+    button.addEventListener('click', skip);
+});
+
+//Could add mousemove event and a click flag to update ranges in real time, not just on a change event. 
+ranges.forEach(function(range){
+    range.addEventListener('change', handleRangeUpdate);
+});
 
 
